32870a2ab437ba484a81757dd41b11745a589206
Shashank Awasthi <shashankawasthi@Genx-Mac-3.local>
2021-02-24 15:46:46 +0530

    Initial commit

---
+++ network/organizations/cryptogen/generateCryptoconfig.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+ export PATH=${PWD}/../../../bin:$PATH
+
+ echo $PATH
+
+ cd /Users/shashankawasthi/Documents/ClosedWalletProject/network/organizations/cryptogen
+
+  infoln "Creating Orderer Org Identities"
+
+    set -x
+    cryptogen generate --config=crypto-config-orderer.yaml --output="organizations"
+    res=$?
+    { set +x; } 2>/dev/null
+    if [ $res -ne 0 ]; then
+      fatalln "Failed to generate certificates..."
+    fi
+
+  infoln "Creating Org1 Identities"
+
+ set -x
+    cryptogen generate --config=crypto-config-org1.yaml --output="organizations"
+    res=$?
+    { set +x; } 2>/dev/null
+    if [ $res -ne 0 ]; then
+      fatalln "Failed to generate certificates..."
+    fi
